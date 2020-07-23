2ced958317739352dcc4a8dd55cfb79b315c9c96
hungth230297@gmail.com <hungth230297@gmail.com>
2020-07-23 17:56:19 +0700

    create_api

---
+++ MyProject.Application/System/User/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using MyProject.Application.ModelRequestService.ServiceRequest.User;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Application.System.User
+{
+    public interface IUserService
+    {
+        Task<bool> Authenticate(LoginRequest request);
+        Task<bool> Register();
+    }
+}
---
+++ MyProject.Application/System/User/UserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Application.System.User
+{
+    public class UserService : IUserService
+    {
+        
+    }
+}
