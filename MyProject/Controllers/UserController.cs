eeda0baad09b92edb5045d3200aa80223761f144
hungth230297@gmail.com <hungth230297@gmail.com>
2020-07-27 18:02:10 +0700

    create view login

---
+++ MyProject/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyProject.Application.ModelRequestService.ServiceRequest.User;
+
+namespace MyProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
